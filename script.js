81739f34f7a0f8b424a31ea12cede3af5c81d1a7
Bill Leonhardt <iamleonhardt@gmail.com>
2016-12-21 14:49:48 -0800

    initial setup

---
+++ script.js
@@ -0,0 +1,50 @@
+
+// CREATES COHORTS
+// Input field that creates an array - cohort
+// Ability to add students to an array
+
+
+
+
+
+// Assign groups for team projects and SAs
+// Randomly splits students from array into selected amount of groups
+// Creates divs per group
+// Can drag drop students to groups
+// Keeps history of chosen groups?
+
+
+
+
+
+// Q&A
+// Chooses group order
+// Chooses random funny name for groups
+// Chooses which student is called on for question
+
+
+
+
+// Random Volunteer
+// Chooses a random student
+// Can un-check which students it rolls
+
+
+
+
+// Random Whiteboard
+// 1-6
+// Displays instructions
+
+
+
+// Drunk cards
+// Pulls random card
+
+
+
+// Sound effect buttons
+// Ding
+// Buzz
+
+
